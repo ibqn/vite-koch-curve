e4a5ac414785860e329a77b9246449f3e8ba69f9
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-25 16:52:18 +0100

    move venv to .venv

--- prepare.bash
+++ prepare.bash
@@ -2,9 +2,9 @@
 
 set -e
 
-python3.12 -m venv './venv'
+python3.12 -m venv '.venv'
 
-source './venv/bin/activate'
+source '.venv/bin/activate'
 
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade setuptools
