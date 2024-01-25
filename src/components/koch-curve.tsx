15bfd71d7a0a72d824c8a5304655acd82c070c4e
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-01-25 15:17:57 +0100

    make mobile friendly

--- src/components/koch-curve.tsx
+++ src/components/koch-curve.tsx
@@ -18,7 +18,7 @@ export const KochCurve = ({ level }: Props) => {
 
   return (
     <svg
-      className="h-[800px] w-[800px]"
+      className="h-[400px] w-[400px] sm:h-[800px] sm:w-[800px]"
       viewBox="0 0 100 100"
       xmlns="http://www.w3.org/2000/svg"
     >
